4d125f666954fbecd3ac7c73ed0bafa7a2f8bb2b
Nikita Chernenkiy <chernenkiy.nikitych136@yandex.ru>
2023-10-10 18:20:32 +0200

    3 Курс Домашнее задание №31 Введение в базы данных

--- src/main/java/sky/pro/course3/homework/ru/hogwarts/school/model/Student.java
+++ src/main/java/sky/pro/course3/homework/ru/hogwarts/school/model/Student.java
@@ -1,8 +1,10 @@
 package sky.pro.course3.homework.ru.hogwarts.school.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
 import java.util.Objects;
 
 @Entity
